848d79da562d993bf0de3f926a3de97dc62d7d60
Agent! for MacOS <todd@starplayrx.com>
2026-08-28 18:58:46 -0400

    Bump deps: AXorcist 0.1.6, AgentAudit 1.3.1 (pulls Commander 0.2.4, swift-log 1.15.0)

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "AgentAccess", targets: ["AgentAccess"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/macOS26/AgentAudit.git", from: "1.0.0"),
-        .package(url: "https://github.com/steipete/AXorcist.git", from: "0.1.0"),
+        .package(url: "https://github.com/macOS26/AgentAudit.git", from: "1.3.1"),
+        .package(url: "https://github.com/steipete/AXorcist.git", from: "0.1.6"),
     ],
     targets: [
         .target(name: "AgentAccess", dependencies: ["AgentAudit", "AXorcist"]),
