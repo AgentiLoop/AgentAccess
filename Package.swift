5f33a06e0efcc50d12c3aced5f6b325ce8f0c42b
Agent! for MacOS <todd@starplayrx.com>
2026-09-15 16:45:11 -0400

    Update AXorcist dependency to 0.1.10

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AgentiLoop/AgentAudit.git", from: "1.3.1"),
-        .package(url: "https://github.com/steipete/AXorcist.git", from: "0.1.9"),
+        .package(url: "https://github.com/steipete/AXorcist.git", from: "0.1.10"),
     ],
     targets: [
         .target(name: "AgentAccess", dependencies: ["AgentAudit", "AXorcist"]),
